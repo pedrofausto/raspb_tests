1aafd21c37e47f913765b335d9c5e3dab4cd0332
pedrofausto <pedrofausto@gmail.com>
2014-09-11 23:36:54 -0300

    First test

--- src/rpi.c
+++ src/rpi.c
@@ -9,5 +9,26 @@
 
 int main (int argc, char* argv[])
 {
-	return 0;
+	if(map_peripheral(&gpio) == -1)
+	{
+		perror("Failed to map the physical GPIO registers into virtual memory.\n");
+	}
+	return -1;
+
+
+/*
+ * Defining pin 8 as output.
+ * This means that we need to change the registers that belongs to GPIO 14
+ */
+
+	INP_GPIO(14);
+	OUT_GPIO(14);
+
+	while(1)
+	{
+		sleep(1);
+		GPIO_CLR = 1 << 14;
+	}
+
+
 }
